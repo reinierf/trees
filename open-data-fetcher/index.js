8a799f6db1b53bf6a8cb7f8ebc38dd19f3b64d5a
rf77 <reinierfeijen@gmail.com>
2026-06-08 15:28:23 +0200

    comment in fetcher

--- open-data-fetcher/index.js
+++ open-data-fetcher/index.js
@@ -12,7 +12,7 @@
  *   node index.js --city groningen --count 5 -d   # dry run: print to console
  *   node index.js --city rotterdam --layer ms:obs_bmn_bijz
  *
- * Available cities: rotterdam, groningen
+ * Available cities: rotterdam, groningen, amsterdam, den-haag
  */
 
 import { CITIES } from './config.js';
